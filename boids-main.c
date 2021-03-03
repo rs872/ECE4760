f91ce191c799da1357324654eef6b30afe0ec9aa
Raghav Kumar <37258607+raghav2956@users.noreply.github.com>
2021-03-03 14:36:15 -0500

    Update boids-main.c

--- boids-main.c
+++ boids-main.c
@@ -37,22 +37,39 @@ float slider_value ; // value could be large
 char receive_string[64];
 
 //Global Accums
-_Accum turnfactor = 0.2;
-_Accum visualRange = 20;
-_Accum protectedRange = 2;
-_Accum centeringfactor = 0.0005;
-_Accum avoidfactor = 0.05;
-_Accum matchingfactor = 0.05;
+_Accum turn_factor = 0.2;
+_Accum visual_range = 20;
+_Accum protected_range = 2;
+_Accum centering_factor = 0.0005;
+_Accum avoid_factor = 0.05;
+_Accum matching_factor = 0.05;
 
 //pixels/frame
-_Accum maxspeed = 3;
-_Accum minspeed = 2;
+_Accum max_speed = 3;
+_Accum min_speed = 2;
 
 //pixels
-_Accum topmargin = 50;
-_Accum bottommargin = 50;
-_Accum leftmargin = 50;
-_Accum rightmargin = 50;
+#define top_margin 50
+#define bottom_margin 50
+#define left_margin 50
+#define right_margin 50
+
+// of boids 
+# define boids 1
+
+//boid arrays
+_Accum x_pos[boids];
+_Accum y_pos[boids];
+_Accum x_vel[boids];
+_Accum y_vel[boids];
+
+#define tft_x 320
+#define tft_y 240
+
+#define float2Accum(a) ((_Accum)(a))
+#define Accum2float(a) ((float)(a))
+#define int2Accum(a) ((_Accum)(a))
+#define Accum2int(a) ((int)(a))
 
 
 /* Demo code for interfacing TFT (ILI9340 controller) to PIC32
@@ -91,6 +108,7 @@ int sys_time_seconds ;
 static PT_THREAD (protothread_timer(struct pt *pt))
 {
     PT_BEGIN(pt);
+
      tft_setCursor(0, 0);
      tft_setTextColor(ILI9340_WHITE);  tft_setTextSize(1);
      tft_writeString("Time in seconds since boot\n");
@@ -113,12 +131,7 @@ static PT_THREAD (protothread_timer(struct pt *pt))
 // === Animation Thread =============================================
 // update a 1 second tick counter
 
-#define float2Accum(a) ((_Accum)(a))
-#define Accum2float(a) ((float)(a))
-#define int2Accum(a) ((_Accum)(a))
-#define Accum2int(a) ((int)(a))
-static _Accum xc=int2Accum(10), yc=int2Accum(150), vxc=int2Accum(2), vyc=0;
-static _Accum g = float2Accum(0.1), drag = float2Accum(.01);
+
 
 static PT_THREAD (protothread_anim(struct pt *pt))
 {
@@ -127,22 +140,120 @@ static PT_THREAD (protothread_anim(struct pt *pt))
       while(1) {
         // yield time 1 second
         PT_YIELD_TIME_msec(32);
+        
+        int i;
+        for (i = 0; i < boids; i++){
+            // erase disk
+            tft_fillCircle(Accum2int(x_pos[i]), Accum2int(y_pos[i]), 4, ILI9340_BLACK); //x, y, radius, color
+            _Accum xpos_avg = 0;
+            _Accum ypos_avg = 0;
+            _Accum xvel_avg = 0;
+            _Accum yvel_avg = 0;
+            _Accum neighboring_boids = 0;
+            _Accum close_dx = 0;
+            _Accum close_dy = 0;
+            
+            int j;
+            for (j = 0; j < boids; j++){
+                if ( j != i ){
+                    
+                    //Distance between boids
+                    _Accum dx = x_pos[i] - x_pos[j];
+                    _Accum dy = y_pos[i] - y_pos[j];
+                    
+                    //Is it in visual range
+                    if (abs(dx) < visual_range && abs(dy) < visual_range){
+                        _Accum squared_distance = dx*dx + dy*dy;
+                        
+                        
+                        //Is squared distance less than the protected range?
+                        if (squared_distance < protected_range*protected_range){
+                            //If so, calculate difference in x/y-coordinates to nearfield boid
+                            close_dx += x_pos[i] - x_pos[j]; 
+                            close_dy += y_pos[i] - y_pos[j];  
+                        }
+                    
+                        //If not in protected range, is the boid in the visual range?
+                        else if (squared_distance < visual_range * visual_range){
 
-        // erase disk
-         tft_fillCircle(Accum2int(xc), Accum2int(yc), 4, ILI9340_BLACK); //x, y, radius, color
-        // compute new velocities
-         vyc = vyc + g - (vyc * drag) ;
-         vxc = vxc - (vxc * drag);
-         
-         xc = xc + vxc;
-         yc = yc + vyc;
-         
-         if (xc<int2Accum(5) || xc>int2Accum(235)) vxc = -vxc; 
-         if (yc>int2Accum(315)) vyc = -vyc; 
-         
-         //  draw disk
-         tft_fillCircle(Accum2int(xc), Accum2int(yc), 4, ILI9340_GREEN); //x, y, radius, color
+                        //Add other boid's x/y-coord and x/y vel to accumulator variables
+                        xpos_avg += x_pos[j];
+                        ypos_avg += y_pos[j];
+                        xvel_avg += x_vel[j];
+                        yvel_avg += y_vel[j];
+
+                        //Increment number of boids within visual range
+                        neighboring_boids += 1;
+                        }
+                        
+
+                    //    # If there were any boids in the visual range . . .            
+                    //    if (neighboring_boids > 0): 
+                    //
+                    //        # Divide accumulator variables by number of boids in visual range
+                    //        xpos_avg = xpos_avg/neighboring_boids 
+                    //        ypos_avg = ypos_avg/neighboring_boids
+                    //        xvel_avg = xvel_avg/neighboring_boids
+                    //        yvel_avg = yvel_avg/neighboring_boids
+                    //
+                    //        # Add the centering/matching contributions to velocity
+                    //        boid.vx = (boid.vx + 
+                    //                   (xpos_avg - boid.x)*centering_factor + 
+                    //                   (xvel_avg - boid.vx)*matching_factor)
+                    //
+                    //        boid.vy = (boid.vy + 
+                    //                   (ypos_avg - boid.y)*centering_factor + 
+                    //                   (yvel_avg - boid.vy)*matching_factor)
+                    //
+                    //    # Add the avoidance contribution to velocity
+                    //    boid.vx = boid.vx + (close_dx*avoidfactor)
+                    //    boid.vy = boid.vy + (close_dy*avoidfactor)
+                    }
+                }
+            }
+            //If the boid is near an edge, make it turn by turnfactor
+            if (y_pos[i] < top_margin){ //Top left corner is (0,0); at top edge, y_pos is min
+                y_vel[i] = y_vel[i] + turn_factor;
+            }
+
+            if (x_pos[i] > tft_x - right_margin){ //At right edge, x_pos is max
+                x_vel[i] = x_vel[i] - turn_factor;
+            }         
+
+            if (x_pos[i] < left_margin){ //at left edge, x_pos is min
+                x_vel[i] = x_vel[i] + turn_factor;
+            }
+
+            if (y_pos[i] > tft_y - bottom_margin){ //At bottom edge, y_pos is max
+                y_vel[i] = y_vel[i] - turn_factor;
+            }                     
+
+
+            // Calculate the boid's speed
+            // Slow step! Lookup the "alpha max plus beta min" algorithm
+
+            _Accum speed = sqrt(x_vel[i]*x_vel[i] + y_vel[i]*y_vel[i]);
+
+            //Enforce min and max speeds
+            if (speed < min_speed) {
+                x_vel[i] = (x_vel[i]/speed)*min_speed;
+                y_vel[i] = (y_vel[i]/speed)*min_speed;
+            }
+            if (speed > max_speed){ 
+                x_vel[i] = (x_vel[i]/speed)*max_speed;
+                y_vel[i] = (y_vel[i]/speed)*max_speed;
+            }
+
+            //Update boid's position
+            x_pos[i] = x_pos[i] + x_vel[i];
+            y_pos[i] = y_pos[i] + y_vel[i];
+        
+        
+        //  draw disk
+        tft_fillCircle(Accum2int(x_pos[i]), Accum2int(y_pos[i]), 4, ILI9340_WHITE); //x, y, radius, color
+      }
         // NEVER exit while
+         
       } // END WHILE(1)
   PT_END(pt);
 } // animation thread
@@ -226,6 +337,27 @@ void main(void) {
   //240x320 vertical display
   tft_setRotation(1); // Use tft_setRotation(1) for 320x240
   
+  _Accum random;
+  //randomize boid arrays
+  int i;
+  srand(5);
+  for (i = 0; i < boids; i++){
+      random = int2Accum(rand() % 320);
+      
+    
+      x_pos[i] = random;
+      printf("This is xpos[0] %d\n", Accum2int(x_pos[0]));
+      random = int2Accum(rand() % 240);
+      
+      y_pos[i] = random;
+      printf("This is ypos[0] %d\n", Accum2int(y_pos[0]));
+      
+      //Velocity algo checks for max speed
+      x_vel[i] = random;
+      y_vel[i] = random;
+    
+  }
+  
   // === config threads ========================
   PT_setup();
   
@@ -257,6 +389,10 @@ void main(void) {
   PT_SCHEDULE(protothread_sched(&pt_sched));
   // ============================================
 } // main
+
+
+
+
 
 
 
