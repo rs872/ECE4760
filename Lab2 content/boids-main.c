93c601adde7aa4c677af97e73970503d6104a94d
Rishi Singhal <13864395+rs872@users.noreply.github.com>
2021-03-20 19:14:57 -0400

    Update boids-main.c

--- Lab2 content/boids-main.c
+++ Lab2 content/boids-main.c
@@ -25,8 +25,10 @@
 ////////////////////////////////////
 #include <math.h>
 
+//macro function to get the absolute value
 #define abs(x) ((x > 0) ? x: -x)
 
+//used for Python GUI
 char new_string = 0;
 char new_slider = 0;
 
@@ -38,7 +40,7 @@ float slider_value ; // value could be large
 // current string
 char receive_string[64];
 
-//Global Accums
+//Global variables to tune boids behavior
 _Accum turn_factor = 0.32;
 _Accum visual_range = 20;
 _Accum protected_range = 10;
@@ -46,28 +48,30 @@ _Accum centering_factor = 0.0005;
 _Accum avoid_factor = 0.05;
 _Accum matching_factor = 0.05;
 
-//pixels/frame
+//speed limits
 _Accum max_speed = 3;
 _Accum min_speed = 2;
 
-//pixels
+//pixels to define ranges in the TFT
 #define top_margin 50
 #define bottom_margin 50
 #define left_margin 50
 #define right_margin 50
 
-// of boids 
+// # of boids 
 # define boids 112
 
-//boid arrays
+//boid arrays for position and velocities for each boid
 _Accum x_pos[boids];
 _Accum y_pos[boids];
 _Accum x_vel[boids];
 _Accum y_vel[boids];
 
+//pixel values for the x and y of the tft
 #define tft_x 320
 #define tft_y 240
 
+//macro to convert between accums,floats and ints
 #define float2Accum(a) ((_Accum)(a))
 #define Accum2float(a) ((float)(a))
 #define int2Accum(a) ((_Accum)(a))
@@ -82,14 +86,12 @@ _Accum y_vel[boids];
   This is an example sketch for the Adafruit 2.2" SPI display.
   This library works with the Adafruit 2.2" TFT Breakout w/SD card
   ----> http://www.adafruit.com/products/1480
-
   Check out the links above for our tutorials and wiring diagrams
   These displays use SPI to communicate, 4 or 5 pins are required to
   interface (RST is optional)
   Adafruit invests time and resources providing this open source code,
   please support Adafruit and open-source hardware by purchasing
   products from Adafruit!
-
   Written by Limor Fried/Ladyada for Adafruit Industries.
   MIT license, all text above must be included in any redistribution
  ****************************************************/
@@ -110,32 +112,27 @@ int sys_time_seconds ;
 static PT_THREAD (protothread_timer(struct pt *pt))
 {
     PT_BEGIN(pt);
-
-     
-     
-     
-
-     while(1) {
+    while(1) {
         // yield time 1 second
         PT_YIELD_TIME_msec(1000) ;
-        tft_fillRect(0,0,100,100,ILI9340_BLACK);
-        
-        sys_time_seconds++ ;
-        tft_setCursor(0, 0);
-        tft_setTextColor(ILI9340_WHITE);  tft_setTextSize(1);
-        //tft_writeString("Time in seconds since boot\n");
+        tft_fillRect(0,0,100,100,ILI9340_BLACK); //Make background black
+        sys_time_seconds++ ; //increment sys time since this thread runs every second
+        tft_setCursor(0, 0); 
+        tft_setTextColor(ILI9340_WHITE); 
+        tft_setTextSize(1);
+        //tft_writeString("Time in seconds since boot\n"); 
         tft_setTextColor(ILI9340_YELLOW); tft_setTextSize(2);
         tft_setCursor(0, 10);
-        sprintf(buffer,"%d", sys_time_seconds);
+        sprintf(buffer,"%d", sys_time_seconds); //write number of seconds to TFT
         tft_writeString(buffer);
         
         tft_setCursor(0, 30);
         //tft_writeString("FPS\n");
-        sprintf(buffer,"%d\n", 30);
+        sprintf(buffer,"%d\n", 30); //write the FPS value to the TFT (30 FPS)
         tft_writeString(buffer);
      
         //tft_writeString("Boids\n");
-        sprintf(buffer,"%d", boids);
+        sprintf(buffer,"%d", boids); // write number of boids to the TFT
         tft_writeString(buffer);
 
         // draw sys_time
@@ -146,22 +143,17 @@ static PT_THREAD (protothread_timer(struct pt *pt))
 
 // === Animation Thread =============================================
 // update a 1 second tick counter
-
-
-
 static PT_THREAD (protothread_anim(struct pt *pt))
 {
-    PT_BEGIN(pt);
-    
-      while(1) {
+    PT_BEGIN(pt);    
+    while(1) {
         // yield time 1 second
-        int begin_time = PT_GET_TIME();
-        
-        
+        int begin_time = PT_GET_TIME(); //get start time
         int i;
-        for (i = 0; i < boids; i++){
+        for (i = 0; i < boids; i++){ //loop through each boid to set its velocity and position
             // erase disk
             tft_drawPixel(Accum2int(x_pos[i]), Accum2int(y_pos[i]), ILI9340_BLACK); //x, y, radius, color
+            //initialize variables for the animation
             _Accum xpos_avg = 0;
             _Accum ypos_avg = 0;
             _Accum xvel_avg = 0;
@@ -171,51 +163,51 @@ static PT_THREAD (protothread_anim(struct pt *pt))
             _Accum close_dy = 0;
             
             int j;
-            for (j = 0; j < boids; j++){
+            //loop through all other boids except for the one iterated on currently
+            for (j = 0; j < boids; j++){ 
                 if ( j != i ){
-                    
                     //Distance between boids
                     _Accum dx = x_pos[i] - x_pos[j];
                     _Accum dy = y_pos[i] - y_pos[j];
                     
-                    //Is it in visual range
+                    //Check if in visual range, soft check without calculating distance to avoid multiplies 
                     if (abs(dx) < visual_range && abs(dy) < visual_range){
                         _Accum squared_distance = dx*dx + dy*dy;
                         
                         
-                        //Is squared distance less than the protected range?
+                        //Check radius is within the protected range
                         if (squared_distance < protected_range*protected_range){
-                            //If so, calculate difference in x/y-coordinates to nearfield boid
+                            //Calculate how far away the boid is for x,y
                             close_dx += x_pos[i] - x_pos[j]; 
                             close_dy += y_pos[i] - y_pos[j];  
                         }
                     
-                        //If not in protected range, is the boid in the visual range?
+                        //Check if distance is actually in visual range
                         else if (squared_distance < visual_range * visual_range){
 
-                        //Add other boid's x/y-coord and x/y vel to accumulator variables
+                        //Add running sum of other boid's velocities and positions
                         xpos_avg += x_pos[j];
                         ypos_avg += y_pos[j];
                         xvel_avg += x_vel[j];
                         yvel_avg += y_vel[j];
 
-                        //Increment number of boids within visual range
+                        //Increment tally of boids nearby (in visual range)
                         neighboring_boids += int2Accum(1);
                         }
                         
                     }
                 }
             }
-            //If there were any boids in the visual range . . .            
+            //If there are nearby boids        
             if (neighboring_boids > 0){
 
-               // Divide accumulator variables by number of boids in visual range
+               // Get the average of the positions of the nearby boids to get a center of mass
                 xpos_avg = xpos_avg/neighboring_boids;
                 ypos_avg = ypos_avg/neighboring_boids;
                 xvel_avg = xvel_avg/neighboring_boids;
                 yvel_avg = yvel_avg/neighboring_boids;
 
-                // Add the centering/matching contributions to velocity
+                // Add the centering/matching factors to velocity
                 x_vel[i] = (x_vel[i] + 
                            (xpos_avg - x_pos[i])*centering_factor + 
                            (xvel_avg - x_vel[i])*matching_factor);
@@ -224,27 +216,10 @@ static PT_THREAD (protothread_anim(struct pt *pt))
                            (ypos_avg - y_pos[i])*centering_factor + 
                            (yvel_avg - y_vel[i])*matching_factor);
               }
-            // Add the avoidance contribution to velocity
+            // Add the avoidance factors to velocity
             x_vel[i] = x_vel[i] + (close_dx*avoid_factor);
             y_vel[i] = y_vel[i] + (close_dy*avoid_factor);
                     
-                
-            
-//            if (y_pos[i] < 0){ //Top left corner is (0,0); at top edge, y_pos is min
-//                y_vel[i] = 0;
-//            }
-//
-//            if (x_pos[i] > tft_x ){ //At right edge, x_pos is max
-//                x_vel[i] = 0;
-//            }         
-//
-//            if (x_pos[i] < 0){ //at left edge, x_pos is min
-//                x_vel[i] = 0;
-//            }
-//
-//            if (y_pos[i] > tft_y){ //At bottom edge, y_pos is max
-//                y_vel[i] = 0;
-//            }
             //If the boid is near an edge, make it turn by turnfactor
             if (y_pos[i] < top_margin){ //Top left corner is (0,0); at top edge, y_pos is min
                 y_vel[i] = y_vel[i] + turn_factor;
@@ -261,24 +236,20 @@ static PT_THREAD (protothread_anim(struct pt *pt))
             if (y_pos[i] > tft_y - bottom_margin){ //At bottom edge, y_pos is max
                 y_vel[i] = y_vel[i] - turn_factor;
             } 
-            
 
-
-            // Calculate the boid's speed
-            // Slow step! Lookup the "alpha max plus beta min" algorithm
-
-            //_Accum speed = sqrt(x_vel[i]*x_vel[i] + y_vel[i]*y_vel[i]);//optimization
+            /* Calculate the boid's speed, using alpha max plus beta min algorithm 
+               to approximate square root */
             _Accum speed;
-            
+            //speed = max + 0.4*min
             if(abs(x_vel[i]) > abs(y_vel[i])) {
-                speed =  abs(x_vel[i]) + 0.4 * abs(y_vel[i]);
+                speed =  abs(x_vel[i]) + 0.4 * abs(y_vel[i]); 
             }
             else  {
-                speed =  abs(y_vel[i]) + 0.4 * abs(x_vel[i]);
+                speed =  abs(y_vel[i]) + 0.4 * abs(x_vel[i]); 
             }
 
-            //Enforce min and max speeds
-            speed = (1/(speed+ 0.01));
+            //Enforce min and max speed limits
+            speed = (1/(speed+ 0.01)); //save reciprocal value to only divide once instead of twice
             if (speed < min_speed) {
                 x_vel[i] = (x_vel[i]*speed)*min_speed;
                 y_vel[i] = (y_vel[i]*speed)*min_speed;
@@ -288,25 +259,23 @@ static PT_THREAD (protothread_anim(struct pt *pt))
                 y_vel[i] = (y_vel[i]*speed)*max_speed;
             }
             
-                   
-            
             //Update boid's position
             x_pos[i] = x_pos[i] + x_vel[i];
             y_pos[i] = y_pos[i] + y_vel[i];
-        
-        
-        //  draw disk
-        tft_drawPixel(Accum2int(x_pos[i]), Accum2int(y_pos[i]), ILI9340_WHITE); //x, y, radius, color
+            //  draw disk
+            tft_drawPixel(Accum2int(x_pos[i]), Accum2int(y_pos[i]), ILI9340_WHITE); //x, y, radius, color
         
       }
         // NEVER exit while
-        if(PT_GET_TIME() - begin_time > 32) mPORTASetBits(BIT_0); 
+        if(PT_GET_TIME() - begin_time > 32) mPORTASetBits(BIT_0); //light LED if failed timing of 30fps
         else mPORTAClearBits(BIT_0);
-        PT_YIELD_TIME_msec(32 - (PT_GET_TIME() - begin_time));
+        PT_YIELD_TIME_msec(32 - (PT_GET_TIME() - begin_time)); //elapsed time in msec
       } // END WHILE(1)
   PT_END(pt);
 } // animation thread
 
+// === Animation Thread =============================================
+// used to update the parameters for the boids algorithm based on slider values on the GUI
 static PT_THREAD (protothread_sliders(struct pt *pt))
 {
     PT_BEGIN(pt);
@@ -314,29 +283,28 @@ static PT_THREAD (protothread_sliders(struct pt *pt))
         PT_YIELD_UNTIL(pt, new_slider==1);
         // clear flag
         new_slider = 0; 
-        if (slider_id == 1){
+        if (slider_id == 1){ // turn factor slider
             turn_factor = slider_value/100.0;
         }
         
-        if (slider_id==2 ){
+        if (slider_id==2 ){ //protected range slider
             protected_range = slider_value;
         }
-        if (slider_id==3 ){
+        if (slider_id==3 ){ //visual range slider
             visual_range = slider_value;
         }
-        if (slider_id==4 ){
+        if (slider_id==4 ){ //centering factor slider 
             centering_factor = slider_value/10000.0;
         }
-        if (slider_id==5 ){
+        if (slider_id==5 ){ //avoid factor slider
             avoid_factor = slider_value/100.0;
         }
-        if (slider_id==6 ){
+        if (slider_id==6 ){ //matching factor slider
             matching_factor = slider_value/100.0;
         }
     }
     PT_END(pt);
 }
-
 
 // === Python serial thread ====================================================
 // you should not need to change this thread UNLESS you add new control types
@@ -344,8 +312,6 @@ static PT_THREAD (protothread_serial(struct pt *pt))
 {
     PT_BEGIN(pt);
     static char junk;
-    //   
-    //
     while(1){
         // There is no YIELD in this loop because there are
         // YIELDS in the spawned threads that determine the 
@@ -391,20 +357,6 @@ static PT_THREAD (protothread_serial(struct pt *pt))
 // === Main  ======================================================
 
 void main(void) {
-   
-//    // control CS for DAC
-//    mPORTBSetPinsDigitalOut(BIT_4);
-//    mPORTBSetBits(BIT_4);
-//    // SCK2 is pin 26 
-//    // SDO2 (MOSI) is in PPS output group 2, could be connected to RB5 which is pin 14
-//    PPSOutput(2, RPB5, SDO2);
-//    // 16 bit transfer CKP=1 CKE=1
-//    // possibles SPI_OPEN_CKP_HIGH;   SPI_OPEN_SMP_END;  SPI_OPEN_CKE_REV
-//    // For any given peripherial, you will need to match these
-//    SpiChnOpen(SPI_CHANNEL2, SPI_OPEN_ON | SPI_OPEN_MODE16 | SPI_OPEN_MSTEN | SPI_OPEN_CKE_REV , 2);
-//  // === end DAC setup =========
-    
-  
   // === setup system wide interrupts  ========
   INTEnableSystemMultiVectoredInt();
   
@@ -420,41 +372,35 @@ void main(void) {
   mPORTAClearBits(BIT_0 );	//Clear bits to ensure light is off.
   mPORTASetPinsDigitalOut(BIT_0);    //Set port as output
   
-  
   _Accum random;
   //randomize boid arrays
   int i;
-  srand(5);
-  for (i = 0; i < boids; i++){
-      random = int2Accum(rand() % 320);
-      
-    
+  srand(5); //seed for random number
+  for (i = 0; i < boids; i++){ //initialize all boids with random position and velocities
+      random = int2Accum(rand() % 320); //make random number is within x range of TFT
       x_pos[i] = random;
-      // Ensure random value generated is within the margins
+      // Ensure random value generated is within the left and right margins
       if (x_pos[i] < left_margin) {
           x_pos[i] = left_margin;
       }
-      
       if (x_pos[i] > tft_x - right_margin) {
           x_pos[i] = tft_x - right_margin;
       }
           
-      random = int2Accum(rand() % 240);
-      
+      random = int2Accum(rand() % 240); //make random number within y range of TFT
       y_pos[i] = random;
       
+      // Ensure random value generated is within the top and bottommargins
       if (y_pos[i] < top_margin) {
           y_pos[i] = top_margin;
       }
-      
       if (y_pos[i] > tft_y - bottom_margin) {
           y_pos[i] = tft_y - bottom_margin;
       }
       
-      //Velocity algo checks for max speed
+      //set random velocity, the Velocity algorithm in animation thread enforces speed limits
       x_vel[i] = random;
       y_vel[i] = random;
-    
   }
   
   // === config threads ========================
@@ -489,6 +435,3 @@ void main(void) {
   PT_SCHEDULE(protothread_sched(&pt_sched));
   // ============================================
 } // main
-
-
-
