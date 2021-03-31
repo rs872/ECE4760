d2637258eec71b7b078750f047f79cbd667e850a
Raghav Kumar <rk524@cornell.edu>
2021-03-31 14:24:10 -0400

    Lab 4

---
+++ Lab-4 content/lab-4.c
@@ -0,0 +1,206 @@
+
+/*
+ * File:        Bird Chirp Synthesizer
+ *             
+ * Author:      Ragav Kumar, Rishi Singhal, and William Salcedo
+ * For use with Sean Carroll's Big Board
+ * http://people.ece.cornell.edu/land/courses/ece4760/PIC32/target_board.html
+ * Target PIC:  PIC32MX250F128B
+ * 
+ * This template instantiates threads to communicate events from a Python
+ * control interface. The python GUI supports push buttons, 
+ * toggle switches (checkbox), Sliders and general text input/putput
+ * 
+ * Start the python script or this program in either order
+ * (The python text is included as a comment at the end of this file)
+ * Clicking on the LED button turns on the on-board LED
+ * Clicking on the Clear LCD button clears the TFT
+ * Clicking the Dot Color checkbox modifies a graphic red/green dot
+ * The slider sets a cursor position on the TFT
+ * The DDS checkbox turns DDS ON/OFF
+ * Scrollig and clicking a Listbox entry will set the DDS waveform if DDS-ON
+ * Typing anything in the Text input line causes the PIC to echo it into the receive window.
+ *   Typing a command of the form "f 400" will result in a 400 Hz sinewave at the DACA output if DDS-ON
+ *   Typing a command of the form "v 1.25" will set that voltage at the DACA output if DDS-OFF
+ *   Typing a command of the form "h" will echo back the form of the other commands
+ * Checking the reset_enable, then clicking RESET PIC does the expected IF the circuit is connected
+ */
+// =============================================
+// NOTE!! -- to use serial spawned functions
+// you MUST EDIT config_1_3_2 to
+// (1) uncomment the line -- #define use_uart_serial
+// (2) SET the baud rate to match the PC terminal
+// =============================================
+////////////////////////////////////
+// clock AND protoThreads configure!
+// You MUST check this file!
+#include "config_1_3_2.h"
+// threading library
+#include "pt_cornell_1_3_2_python.h"
+#include <math.h>
+
+////////////////////////////////////
+// graphics libraries
+// SPI channel 1 connections to TFT
+#include "tft_master.h"
+#include "tft_gfx.h"
+
+// current string
+char receive_string[64];
+
+//used for Python GUI
+char new_string = 0;
+char new_slider = 0;
+
+// current slider
+int slider_id;
+float slider_value ; // value could be large
+
+// === Slider Thread =============================================
+// 
+static PT_THREAD (protothread_sliders(struct pt *pt))
+{
+    PT_BEGIN(pt);
+    while(1){
+        PT_YIELD_UNTIL(pt, new_slider==1);
+        // clear flag
+        new_slider = 0; 
+        if (slider_id == 1){ // turn factor slider
+            printf("hello Yo");
+            OC1RS = slider_value;
+        }
+        else if (slider_id == 2){
+            OC2RS = slider_value;
+        }
+    }
+    PT_END(pt);
+}
+// === Python serial thread ====================================================
+// you should not need to change this thread UNLESS you add new control types
+static PT_THREAD (protothread_serial(struct pt *pt))
+{
+    PT_BEGIN(pt);
+    static char junk;
+    while(1){
+        // There is no YIELD in this loop because there are
+        // YIELDS in the spawned threads that determine the 
+        // execution rate while WAITING for machine input
+        // =============================================
+        // NOTE!! -- to use serial spawned functions
+        // you MUST edit config_1_3_2 to
+        // (1) uncomment the line -- #define use_uart_serial
+        // (2) SET the baud rate to match the PC terminal
+        // =============================================
+        
+        // now wait for machine input from python
+        // Terminate on the usual <enter key>
+        PT_terminate_char = '\r' ; 
+        PT_terminate_count = 0 ; 
+        PT_terminate_time = 0 ;
+
+        // note that there will NO visual feedback using the following function
+        PT_SPAWN(pt, &pt_input, PT_GetMachineBuffer(&pt_input) );
+        
+        // Parse the string from Python
+        // There can be toggle switch, button, slider, and string events
+        
+        // slider
+        if (PT_term_buffer[0]=='s'){
+            sscanf(PT_term_buffer, "%c %d %f", &junk, &slider_id, &slider_value);
+            new_slider = 1;
+        }
+
+        
+        // string from python input line
+        if (PT_term_buffer[0]=='$'){
+            // signal parsing thread
+            new_string = 1;
+            // output to thread which parses the string
+            // while striping off the '$'
+            strcpy(receive_string, PT_term_buffer+1);
+        }                                  
+    } // END WHILE(1)   
+    PT_END(pt);  
+} // thread blink
+
+// === Main  ======================================================
+
+void main(void) {
+    
+    OpenTimer23(T2_ON | T2_PS_1_1 | T2_32BIT_MODE_ON, 800000); //800K (cycles) in hex
+    
+    OpenOC3(OC_ON | OC_TIMER_MODE32 | OC_TIMER2_SRC | OC_PWM_FAULT_PIN_DISABLE , 40000, 40000) ;
+    OpenOC4(OC_ON | OC_TIMER_MODE32 | OC_TIMER2_SRC | OC_PWM_FAULT_PIN_DISABLE , 40000, 40000) ;
+    
+    PPSOutput(4, RPA3, OC3) ;  // configure OC3 to RPA3
+    PPSOutput(3, RPA2, OC4) ;  // configure OC4 to RPA2
+    
+    SetChanADC10( ADC_CH0_NEG_SAMPLEA_NVREF | ADC_CH0_POS_SAMPLEA_AN11 ); // configure to sample AN11
+    
+    
+    
+    
+    
+//  // === set up DAC on big board ========
+//  // timer interrupt //////////////////////////
+//    // Set up timer2 on,  interrupts, internal clock, prescalar 1, toggle rate
+//    #define TIMEOUT (40000000/Fs) // clock rate / sample rate
+//    // 2000 is 20 ksamp/sec
+//    printf("in main");
+//    OpenTimer2(T2_ON | T2_SOURCE_INT | T2_PS_1_1, TIMEOUT);
+//
+//    // set up the timer interrupt with a priority of 2
+//    ConfigIntTimer2(T2_INT_ON | T2_INT_PRIOR_2);
+    mT3ClearIntFlag(); // and clear the interrupt flag
+//
+//    // control CS for DAC
+//    mPORTBSetPinsDigitalOut(BIT_4);
+//    mPORTBSetBits(BIT_4);
+//    // SCK2 is pin 26 
+//    // SDO2 (MOSI) is in PPS output group 2, could be connected to RB5 which is pin 14
+//    PPSOutput(2, RPB5, SDO2);
+//    // 16 bit transfer CKP=1 CKE=1
+//    // possibles SPI_OPEN_CKP_HIGH;   SPI_OPEN_SMP_END;  SPI_OPEN_CKE_REV
+//    // For any given peripherial, you will need to match these
+//    SpiChnOpen(SPI_CHANNEL2, SPI_OPEN_ON | SPI_OPEN_MODE16 | SPI_OPEN_MSTEN | SPI_OPEN_CKE_REV , 2);
+//  // === end DAC setup =========
+//    
+
+// 
+//    // === setup system wide interrupts  ========
+  INTEnableSystemMultiVectoredInt();
+
+  // === config threads ========================
+  PT_setup();
+//  
+//  // === identify the threads to the scheduler =====
+//  // add the thread function pointers to be scheduled
+//  // --- Two parameters: function_name and rate. ---
+//  // rate=0 fastest, rate=1 half, rate=2 quarter, rate=3 eighth, rate=4 sixteenth,
+//  // rate=5 or greater DISABLE thread!
+//  
+//  pt_add(protothread_buttons, 0);
+  pt_add(protothread_serial, 0);
+  pt_add(protothread_sliders, 0);
+//  pt_add(protothread_python_string, 0);
+
+//  
+  // === initalize the scheduler ====================
+  PT_INIT(&pt_sched) ;
+  // >>> CHOOSE the scheduler method: <<<
+  // (1)
+  // SCHED_ROUND_ROBIN just cycles thru all defined threads  
+  // NOTE the controller must run in SCHED_ROUND_ROBIN mode
+  // ALSO note that the scheduler is modified to cpy a char
+  // from uart1 to uart2 for the controller
+  
+  pt_sched_method = SCHED_ROUND_ROBIN ;
+  
+  // === scheduler thread =======================
+  // scheduler never exits
+  PT_SCHEDULE(protothread_sched(&pt_sched));
+  // ============================================
+  
+} // main
+
+
---
+++ Lab3 content/lab-3.c
@@ -0,0 +1,538 @@
+/*
+ * File:        ADC > FFT > TFT
+ * Author:      Bruce Land
+ * 
+ * Target PIC:  PIC32MX250F128B
+ */
+
+////////////////////////////////////
+// clock AND protoThreads configure!
+// You MUST check this file!
+#include "config_1_3_2.h"
+// threading library
+#include "pt_cornell_1_3_2_python.h"
+
+////////////////////////////////////
+// graphics libraries
+#include "tft_master.h"
+#include "tft_gfx.h"
+// need for rand function
+#include <stdlib.h>
+#include <math.h>
+////////////////////////////////////
+
+
+/* Demo code for interfacing TFT (ILI9340 controller) to PIC32
+ * The library has been modified from a similar Adafruit library
+ */
+// Adafruit data:
+/***************************************************
+  This is an example sketch for the Adafruit 2.2" SPI display.
+  This library works with the Adafruit 2.2" TFT Breakout w/SD card
+  ----> http://www.adafruit.com/products/1480
+
+  Check out the links above for our tutorials and wiring diagrams
+  These displays use SPI to communicate, 4 or 5 pins are required to
+  interface (RST is optional)
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  MIT license, all text above must be included in any redistribution
+ ****************************************************/
+
+
+//used for Python GUI
+char new_string = 0;
+char new_slider = 0;
+
+// current slider
+int slider_id;
+float slider_value ; // value could be large
+
+
+// current string
+char receive_string[64];
+// === the fixed point macros ========================================
+#define float2Accum(a) ((_Accum)(a))
+#define Accum2float(a) ((float)(a))
+#define int2Accum(a) ((_Accum)(a))
+#define Accum2int(a) ((int)(a))
+#define abs(x) ((x > 0) ? x: -x)
+// === input arrays ==================================================
+#define nSamp 512
+#define nPixels 256 //slightly higher than height of the screen
+
+//=== sample rate ===========================
+int sample_rate = 11000;
+int timer_match;
+
+
+
+short int v_in[nSamp] ;
+
+// === thread structures ============================================
+// thread control structs
+// note that UART input and output are threads
+static struct pt pt_fft ;
+
+// string buffer
+char buffer[60];
+
+// system 1 second interval tick
+int sys_time_seconds ;
+
+//current x position
+short int current_x = 50;
+
+// === print line for TFT ============================================
+// print a line on the TFT
+// string buffer
+char buffer[60];
+void printLine(int line_number, char* print_buffer, short text_color, short back_color){
+    // line number 0 to 31 
+    /// !!! assumes tft_setRotation(0);
+    // print_buffer is the string to print
+    int v_pos;
+    v_pos = line_number * 10 ;
+    // erase the pixels
+    tft_fillRoundRect(0, v_pos, 239, 8, 1, back_color);// x,y,w,h,radius,color
+    tft_setTextColor(text_color); 
+    tft_setCursor(0, v_pos);
+    tft_setTextSize(1);
+    tft_writeString(print_buffer);
+}
+
+void printLine2(float line_number, char* print_buffer, short text_color, short back_color){
+    // line number 0 to 31 
+    /// !!! assumes tft_setRotation(0);
+    // print_buffer is the string to print
+    int v_pos;
+    v_pos = line_number * 20 ;
+    // erase the pixels
+    tft_fillRoundRect(0, v_pos, 40, 16, 1, back_color);// x,y,w,h,radius,color
+    tft_setTextColor(text_color); 
+    tft_setCursor(0, v_pos);
+    tft_setTextSize(2);
+    tft_writeString(print_buffer);
+}
+
+void printLineXY(int x, float line_number, char* print_buffer, short text_color, short back_color){
+    // line number 0 to 31 
+    /// !!! assumes tft_setRotation(0);
+    // print_buffer is the string to print
+    int v_pos;
+    v_pos = line_number * 20 ;
+    // erase the pixels
+    tft_fillRoundRect(x, v_pos, 239, 16, 1, back_color);// x,y,w,h,radius,color
+    tft_setTextColor(text_color); 
+    tft_setCursor(x, v_pos);
+    tft_setTextSize(2);
+    tft_writeString(print_buffer);
+}
+
+//=== FFT ==============================================================
+// FFT
+#define N_WAVE          512    /* size of FFT 512 */
+#define LOG2_N_WAVE     9     /* log2(N_WAVE) 0 */
+#define begin {
+#define end }
+
+_Accum Sinewave[N_WAVE]; // a table of sines for the FFT
+_Accum window[N_WAVE]; // a table of window values for the FFT
+_Accum fr[N_WAVE], fi[N_WAVE];
+int pixels[nPixels] ;
+
+inline unsigned short getColor(_Accum magnitude) { //convert to colors
+    if(magnitude < int2Accum(1)){
+        return 0x0000;
+    }
+    else if(magnitude < int2Accum(2)){
+        return 0x2945;
+    }
+    else if(magnitude < int2Accum(4)){
+        return 0x4a49;
+    }
+    else if(magnitude < int2Accum(8)){
+        return 0x738e;
+    }
+    else if(magnitude < int2Accum(16)){
+        return 0x85cl;
+    }
+    else if(magnitude < int2Accum(32)){
+        return 0xad55;
+    }
+    else if(magnitude < int2Accum(64)){
+        return 0xc638;
+    }
+    else {
+        return 0xFFFF;
+    }
+    
+   
+}
+
+void FFTaccum(_Accum fr[], _Accum fi[], int m)
+//Adapted from code by:
+//Tom Roberts 11/8/89 and Malcolm Slaney 12/15/94 malcolm@interval.com
+//fr[n],fi[n] are real,imaginary arrays, INPUT AND RESULT.
+//size of data = 2**m
+// This routine does foward transform only
+begin
+    int mr,nn,i,j,L,k,istep, n;
+    _Accum qr,qi,tr,ti,wr,wi;
+
+    mr = 0;
+    n = 1<<m;   //number of points
+    nn = n - 1;
+
+    /* decimation in time - re-order data */
+    for(m=1; m<=nn; ++m)
+    begin
+        L = n;
+        do L >>= 1; while(mr+L > nn);
+        mr = (mr & (L-1)) + L;
+        if(mr <= m) continue;
+        tr = fr[m];
+        fr[m] = fr[mr];
+        fr[mr] = tr;
+        //ti = fi[m];   //for real inputs, don't need this
+        //fi[m] = fi[mr];
+        //fi[mr] = ti;
+    end
+
+    L = 1;
+    k = LOG2_N_WAVE-1;
+    while(L < n)
+    begin
+        istep = L << 1;
+        for(m=0; m<L; ++m)
+        begin
+            j = m << k;
+            wr =  Sinewave[j+N_WAVE/4];
+            wi = -Sinewave[j];
+            //wr >>= 1; do need if scale table
+            //wi >>= 1;
+
+            for(i=m; i<n; i+=istep)
+            begin
+                j = i + L;
+                tr = (wr * fr[j]) - (wi * fi[j]);
+                ti = (wr * fi[j]) + (wi * fr[j]);
+                qr = fr[i] >> 1;
+                qi = fi[i] >> 1;
+                fr[j] = qr - tr;
+                fi[j] = qi - ti;
+                fr[i] = qr + tr;
+                fi[i] = qi + ti;
+            end
+        end
+        --k;
+        L = istep;
+    end
+end
+
+// === FFT Thread =============================================
+    
+// DMA channel busy flag
+#define CHN_BUSY 0x80
+#define log_min 0x10   
+static PT_THREAD (protothread_fft(struct pt *pt))
+{
+    PT_BEGIN(pt);
+    
+    static int sample_number ;
+    static _Accum zero_point_4 = float2Accum(0.4) ;
+    // approx log calc ;
+    static int sx, y, ly, temp ;
+    
+    while(1) {
+        // yield time 1 second
+        PT_YIELD_TIME_msec(30);
+        
+        // enable ADC DMA channel and get
+        // 512 samples from ADC
+        DmaChnEnable(1); //enable channel 1
+        // yield until DMA done: while((DCH0CON & Chn_busy) ){};
+        PT_WAIT_WHILE(pt, DCH1CON & CHN_BUSY);
+        mPORTASetBits(BIT_0 );	//set bits to turn light on.
+        // profile fft time
+        WriteTimer4(0);
+        
+        // compute and display fft
+        // load input array
+        for (sample_number=0; sample_number<nSamp-1; sample_number++){
+            // window the input and perhaps scale it
+            fr[sample_number] = (int2Accum(v_in[sample_number]) * window[sample_number]); 
+            fi[sample_number] = 0 ;
+        }
+        
+        // do FFT
+        FFTaccum(fr, fi, LOG2_N_WAVE);
+        // get magnitude and log
+        // The magnitude of the FFT is approximated as: 
+        //   |amplitude|=max(|Re|,|Im|)+0.4*min(|Re|,|Im|). 
+        // This approximation is accurate within about 4% rms.
+        // https://en.wikipedia.org/wiki/Alpha_max_plus_beta_min_algorithm
+        for (sample_number = 0; sample_number < nPixels; sample_number++) {  
+            // get the approx magnitude
+            fr[sample_number] = abs(fr[sample_number]); //>>9
+            fi[sample_number] = abs(fi[sample_number]);
+            // reuse fr to hold magnitude
+            fr[sample_number] = max(fr[sample_number], fi[sample_number]) + 
+                    (min(fr[sample_number], fi[sample_number]) * zero_point_4); 
+        }
+            
+        int sample_incr = sample_rate / 8; //amount for each tick for frequency (sample_rate/2)/4
+        
+        sprintf(buffer, "%d", sample_incr * 4 );
+        printLine2(0.5, buffer, ILI9340_WHITE, ILI9340_BLACK);
+        
+        sprintf(buffer, "%d", sample_incr * 3 );
+        printLine2(3.5, buffer, ILI9340_WHITE, ILI9340_BLACK);
+        
+        sprintf(buffer, "%d", sample_incr * 2 );
+        printLine2(6.5, buffer, ILI9340_WHITE, ILI9340_BLACK);
+        
+        sprintf(buffer, "%d", sample_incr);
+        printLine2(9.5, buffer, ILI9340_WHITE, ILI9340_BLACK);
+        
+
+        
+
+        // Display on TFT
+        // erase, then draw
+        tft_fillRect(current_x,0,10,240,ILI9340_BLACK);
+        int largest_bin = 6;
+        for (sample_number = 1; sample_number < 240; sample_number++) {
+            //tft_drawPixel(current_x, 240 - sample_number, ILI9340_BLACK); //clear screen
+            
+            tft_drawPixel(current_x, 240 - sample_number, getColor(fr[sample_number]) );
+            if(fr[sample_number] > fr[largest_bin] && sample_number > 5){
+                largest_bin = sample_number;
+            }
+            // reuse fr to hold magnitude 
+
+        }  
+        sprintf(buffer, "Max Freq: %d", sample_rate * largest_bin / 512);
+        printLineXY(50, 0, buffer, ILI9340_WHITE, ILI9340_BLACK);
+
+        
+        current_x++;
+        
+        if(current_x == 320){
+            current_x = 50;
+        }
+        // NEVER exit while
+        
+      } // END WHILE(1)
+  PT_END(pt);
+} // FFT thread
+
+
+
+// === Slider Thread =============================================
+// 
+static PT_THREAD (protothread_sliders(struct pt *pt))
+{
+    PT_BEGIN(pt);
+    while(1){
+        PT_YIELD_UNTIL(pt, new_slider==1);
+        // clear flag
+        new_slider = 0; 
+        if (slider_id == 1){ // turn factor slider
+            sample_rate = slider_value * 2;
+            timer_match = 40000000/sample_rate;
+            CloseTimer3();
+            OpenTimer3(T3_ON | T3_SOURCE_INT | T3_PS_1_1, timer_match); 
+            printf("Hello There");
+        }
+    }
+    PT_END(pt);
+}
+
+// === Python serial thread ====================================================
+// you should not need to change this thread UNLESS you add new control types
+static PT_THREAD (protothread_serial(struct pt *pt))
+{
+    PT_BEGIN(pt);
+    static char junk;
+    while(1){
+        // There is no YIELD in this loop because there are
+        // YIELDS in the spawned threads that determine the 
+        // execution rate while WAITING for machine input
+        // =============================================
+        // NOTE!! -- to use serial spawned functions
+        // you MUST edit config_1_3_2 to
+        // (1) uncomment the line -- #define use_uart_serial
+        // (2) SET the baud rate to match the PC terminal
+        // =============================================
+        
+        // now wait for machine input from python
+        // Terminate on the usual <enter key>
+        PT_terminate_char = '\r' ; 
+        PT_terminate_count = 0 ; 
+        PT_terminate_time = 0 ;
+
+        // note that there will NO visual feedback using the following function
+        PT_SPAWN(pt, &pt_input, PT_GetMachineBuffer(&pt_input) );
+        
+        // Parse the string from Python
+        // There can be toggle switch, button, slider, and string events
+        
+        // slider
+        if (PT_term_buffer[0]=='s'){
+            sscanf(PT_term_buffer, "%c %d %f", &junk, &slider_id, &slider_value);
+            new_slider = 1;
+        }
+
+        
+        // string from python input line
+        if (PT_term_buffer[0]=='$'){
+            // signal parsing thread
+            new_string = 1;
+            // output to thread which parses the string
+            // while striping off the '$'
+            strcpy(receive_string, PT_term_buffer+1);
+        }                                  
+    } // END WHILE(1)   
+    PT_END(pt);  
+} // thread blink
+
+// === Main  ======================================================
+
+void main(void) {
+
+    ANSELA = 0; 
+    ANSELB = 0;
+    // === config threads ========================
+    // turns OFF UART support and debugger pin, unless defines are set
+    PT_setup();
+    // === setup system wide interrupts  ========
+    INTEnableSystemMultiVectoredInt();
+
+    // the ADC ///////////////////////////////////////
+    
+    // timer 3 setup for adc trigger  ==============================================
+    // Set up timer3 on,  no interrupts, internal clock, prescalar 1, compare-value
+    // This timer generates the time base for each ADC sample. 
+    // works at ??? Hz
+    // 40 MHz PB clock rate
+    timer_match = 40000000/sample_rate;
+    OpenTimer3(T3_ON | T3_SOURCE_INT | T3_PS_1_1, timer_match); 
+    
+    //=== DMA Channel 0 transfer ADC data to array v_in ================================
+    // Open DMA Chan1 for later use sending video to TV
+    #define DMAchan1 1
+	DmaChnOpen(DMAchan1, 0, DMA_OPEN_DEFAULT);
+    DmaChnSetTxfer(DMAchan1, (void*)&ADC1BUF0, (void*)v_in, 2, nSamp*2, 2); //512 16-bit integers
+    DmaChnSetEventControl(DMAchan1, DMA_EV_START_IRQ(28)); // 28 is ADC done
+    // ==============================================================================
+    
+    // configure and enable the ADC
+    CloseADC10(); // ensure the ADC is off before setting the configuration
+
+    // define setup parameters for OpenADC10
+    // Turn module on | ouput in integer | trigger mode auto | enable autosample
+    // ADC_CLK_AUTO -- Internal counter ends sampling and starts conversion (Auto convert)
+    // ADC_AUTO_SAMPLING_ON -- Sampling begins immediately after last conversion completes; SAMP bit is automatically set
+    // ADC_AUTO_SAMPLING_OFF -- Sampling begins with AcquireADC10();
+    #define PARAM1  ADC_FORMAT_INTG16 | ADC_CLK_TMR | ADC_AUTO_SAMPLING_ON //
+
+    // define setup parameters for OpenADC10
+    // ADC ref external  | disable offset test | disable scan mode | do 1 sample | use single buf | alternate mode off
+    #define PARAM2  ADC_VREF_AVDD_AVSS | ADC_OFFSET_CAL_DISABLE | ADC_SCAN_OFF | ADC_SAMPLES_PER_INT_1 | ADC_ALT_BUF_OFF | ADC_ALT_INPUT_OFF
+    //
+    // Define setup parameters for OpenADC10
+    // for a 40 MHz PB clock rate
+    // use peripherial bus clock | set sample time | set ADC clock divider
+    // ADC_CONV_CLK_Tcy should work at 40 MHz.
+    // ADC_SAMPLE_TIME_6 seems to work with a source resistance < 1kohm
+    #define PARAM3 ADC_CONV_CLK_PB | ADC_SAMPLE_TIME_6 | ADC_CONV_CLK_Tcy //ADC_SAMPLE_TIME_5| ADC_CONV_CLK_Tcy2
+
+    // define setup parameters for OpenADC10
+    // set AN11 and  as analog inputs
+    #define PARAM4	ENABLE_AN11_ANA // pin 24
+
+    // define setup parameters for OpenADC10
+    // do not assign channels to scan
+    #define PARAM5	SKIP_SCAN_ALL
+
+    // use ground as neg ref for A | use AN11 for input A     
+    // configure to sample AN11 
+    SetChanADC10(ADC_CH0_NEG_SAMPLEA_NVREF | ADC_CH0_POS_SAMPLEA_AN11); // configure to sample AN4 
+    OpenADC10(PARAM1, PARAM2, PARAM3, PARAM4, PARAM5); // configure ADC using the parameters defined above
+
+    EnableADC10(); // Enable the ADC
+    ///////////////////////////////////////////////////////
+
+    // timer 4 setup for profiling code ===========================================
+    // Set up timer2 on,  interrupts, internal clock, prescalar 1, compare-value
+    // This timer generates the time base for each horizontal video line
+    OpenTimer4(T4_ON | T4_SOURCE_INT | T4_PS_1_8, 0xffff);
+        
+    // === init FFT data =====================================
+    // one cycle sine table
+    //  required for FFT
+    int ii;
+    for (ii = 0; ii < N_WAVE; ii++) {
+        Sinewave[ii] = float2Accum(sin(6.283 * ((float) ii) / N_WAVE)*0.5);
+        window[ii] = float2Accum(1.0 * (1.0 - cos(6.283 * ((float) ii) / (N_WAVE - 1))));
+        //window[ii] = float2fix(1.0) ;
+    }
+
+    // === TFT setup ============================
+    // init the display in main since more than one thread uses it.
+    // NOTE that this init assumes SPI channel 1 connections
+    tft_init_hw();
+    tft_begin();
+    tft_fillScreen(ILI9340_BLACK);
+    //240x320 vertical display
+    tft_setRotation(1); // Use tft_setRotation(1) for 320x240
+
+    mPORTAClearBits(BIT_0 );	//Clear bits to ensure light is off.
+    mPORTASetPinsDigitalOut(BIT_0);    //Set port as output
+
+    // Add in ticks and axis for the spectrogram, permanently drawn
+    tft_fillRect(48,0,1,240,ILI9340_RED); //vertical line
+    tft_fillRect(30,240-2,18,2,ILI9340_RED); //bottom tick
+    tft_fillRect(30,240-62,18,2,ILI9340_RED); //2nd tick
+    tft_fillRect(30,240-122,18,2,ILI9340_RED); //3rd tick
+    tft_fillRect(30,240-182,18,2,ILI9340_RED); //4th tick
+    tft_fillRect(30,0,18,2,ILI9340_RED); //top tick
+
+    
+    
+    
+    // === identify the threads to the scheduler =====
+    // add the thread function pointers to be scheduled
+    // --- Two parameters: function_name and rate. ---
+    // rate=0 fastest, rate=1 half, rate=2 quarter, rate=3 eighth, rate=4 sixteenth,
+    // rate=5 or greater DISABLE thread!
+
+    pt_add(protothread_serial, 0);
+    pt_add(protothread_fft,0);
+    pt_add(protothread_sliders,0);
+    // === initalize the scheduler ====================
+    PT_INIT(&pt_sched) ;
+    // >>> CHOOSE the scheduler method: <<<
+    // (1)
+    // SCHED_ROUND_ROBIN just cycles thru all defined threads
+    //pt_sched_method = SCHED_ROUND_ROBIN ;
+
+    // NOTE the controller must run in SCHED_ROUND_ROBIN mode
+    // ALSO note that the scheduler is modified to cpy a char
+    // from uart1 to uart2 for the controller
+
+    pt_sched_method = SCHED_ROUND_ROBIN ;
+
+    
+    
+    // === scheduler thread =======================
+    // scheduler never exits
+    PT_SCHEDULE(protothread_sched(&pt_sched));
+    // ============================================
+    
+
+} // main
